317d3b973eb1425c56f29203544a969aa9fa0fbc
Joschua Schneider <joschua-schneider@web.de>
2019-05-24 10:51:27 +0200

    new: minimal babel config

---
+++ babel/babel.config.js
@@ -0,0 +1,8 @@
+/**
+ * Minimal babel configuration using babel env preset,
+ * For testing and use with react, add @babel/preset-react to presets
+ */
+
+const presets = ["@babel/preset-env"]
+
+module.exports = { presets }
